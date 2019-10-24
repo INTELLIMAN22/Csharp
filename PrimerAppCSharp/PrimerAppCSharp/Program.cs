0d11e13d4b6c680c21a9755a8daf34514ead83c1
Gio Soriano <Gio Soriano@DESKTOP-V9QH2Q2>
2019-10-24 14:01:02 -0500

    Inicio CSharp

---
+++ PrimerAppCSharp/PrimerAppCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimerAppCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Gio, Bienvenido");
+        }
+    }
+}
